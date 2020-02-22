50751b451b5a11ca5c499e9dabb68677a39972bb
Sean Belverstone <seanbelverstone@gmail.com>
2020-02-22 14:05:15 -0600

    Adding values to the dropDown menu. Removed all other options as i will add them individually

--- client/src/components/OrderForm.js
+++ client/src/components/OrderForm.js
@@ -1,79 +1,34 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-const Example = (props) => {
-  return (
-    <Form>
-      <FormGroup>
-        <Label for="exampleEmail">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelect">Select</Label>
-        <Input type="select" name="select" id="exampleSelect">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Input>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelectMulti">Select Multiple</Label>
-        <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Input>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleText">Text Area</Label>
-        <Input type="textarea" name="text" id="exampleText" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleFile">File</Label>
-        <Input type="file" name="file" id="exampleFile" />
-        <FormText color="muted">
-          This is some placeholder block-level help text for the above input.
-          It's a bit lighter and easily wraps to a new line.
-        </FormText>
-      </FormGroup>
-      <FormGroup tag="fieldset">
-        <legend>Radio Buttons</legend>
-        <FormGroup check>
-          <Label check>
-            <Input type="radio" name="radio1" />{' '}
-            Option one is this and that—be sure to include why it's great
-          </Label>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="radio" name="radio1" />{' '}
-            Option two can be something else and selecting it will deselect option one
-          </Label>
-        </FormGroup>
-        <FormGroup check disabled>
-          <Label check>
-            <Input type="radio" name="radio1" disabled />{' '}
-            Option three is disabled
-          </Label>
-        </FormGroup>
-      </FormGroup>
-      <FormGroup check>
-        <Label check>
-          <Input type="checkbox" />{' '}
-          Check me out
-        </Label>
-      </FormGroup>
-      <Button>Submit</Button>
-    </Form>
-  );
+
+
+class OrderForm extends React.Component {
+
+	state = {
+		quantity: 1,
+		totalPrice: 0
+	}
+
+	render() {
+		return (
+			<Form>
+			<FormGroup style={{width: "150px"}}>
+				<Label for="exampleSelect">Quantity</Label>
+				<Input type="select" name="select" id="quantity" value={this.state.quantity}>
+				<option value="1">1 dozen</option>
+				<option value="2">2 dozens</option>
+				<option value="3">3 dozens</option>
+				<option value="4">4 dozens</option>
+				</Input>
+				<div>Price</div>
+				<div>$30.00</div>
+				<div>Total</div>
+				<div id="totalPrice">{this.state.totalPrice}</div>
+			</FormGroup>
+			</Form>
+		);
+	}
 }
 
-export default Example;
+export default OrderForm;
