76da705eece55ca2a6ec98130ac540716664735c
Sean Belverstone <seanbelverstone@gmail.com>
2020-02-22 14:31:07 -0600

    Finished first row of form. Made it so the total updates when an amount of biscuits is selected.

--- client/src/components/OrderForm.js
+++ client/src/components/OrderForm.js
@@ -2,29 +2,55 @@ import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 
+var totalPrice = 30;
 
 class OrderForm extends React.Component {
 
 	state = {
 		quantity: 1,
-		totalPrice: 0
 	}
+
+	handleChange = (event) => {
+		const {name, value} = event.target;
+		this.setState(
+		  {[name]: value}
+		)
+	  };
+
+	componentDidUpdate = () => {
+		this.handleTotal(this.state.quantity)
+	}
+
+	handleTotal = (quantity) => {
+		totalPrice = parseInt(this.state.quantity)*30
+	};
+	
 
 	render() {
 		return (
 			<Form>
 			<FormGroup style={{width: "150px"}}>
-				<Label for="exampleSelect">Quantity</Label>
-				<Input type="select" name="select" id="quantity" value={this.state.quantity}>
-				<option value="1">1 dozen</option>
-				<option value="2">2 dozens</option>
-				<option value="3">3 dozens</option>
-				<option value="4">4 dozens</option>
+				<Label for="quantity">Quantity</Label>
+				<Input 
+					type="select" 
+					name="quantity" 
+					id="quantity" 
+					value={this.state.quantity}
+					onChange={this.handleChange}
+					on={this.handleTotal(this.state.quantity)}>
+
+					<option value="1">1 dozen</option>
+					<option value="2">2 dozens</option>
+					<option value="3">3 dozens</option>
+					<option value="4">4 dozens</option>
+
 				</Input>
-				<div>Price</div>
-				<div>$30.00</div>
-				<div>Total</div>
-				<div id="totalPrice">{this.state.totalPrice}</div>
+
+				<Label for="price">Price</Label>
+				<div id="price" name="price">$30.00</div>
+				<Label for="totalPrice">Total</Label>
+				<div id="totalPrice" name="totalPrice">${totalPrice}</div>
+
 			</FormGroup>
 			</Form>
 		);
