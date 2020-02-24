210ca33d93683e65ca93bbd285e451fde5618d5e
Sean Belverstone <seanbelverstone@gmail.com>
2020-02-23 20:27:20 -0600

    Discovered that I need date-fns for the date-picker to work. Installed and have now set a minimum date for collection. Going to add in a time selector too

--- client/src/components/UserCalendar.js
+++ client/src/components/UserCalendar.js
@@ -1,30 +1,32 @@
 import React from "react";
 import DatePicker from "react-datepicker";
- 
+import subDays from "date-fns/addDays";
+
 import "react-datepicker/dist/react-datepicker.css";
  
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
  
 class UserCalendar extends React.Component {
-  state = {
-    startDate: new Date()
-  };
- 
-  handleChange = date => {
-    this.setState({
-      startDate: date
-    });
-  };
- 
-  render() {
-    return (
-      <DatePicker
-        selected={this.state.startDate}
-        onChange={this.handleChange}
-      />
-    );
-  }
-}
+	state = {
+		startDate: new Date()
+	  };
+	 
+	  handleChange = date => {
+		this.setState({
+		  startDate: date
+		});
+	  };
+	 
+	  render() {
+		return (
+		  <DatePicker
+			selected={this.state.startDate}
+			onChange={this.handleChange}
+			minDate={subDays(new Date(), 2)}
+			/>
+		);
+	  }
+	}
 
 export default UserCalendar;
