e74f6978d137fc3f17ab8c1f34b94a8fd56fd54c
Sean Belverstone <seanbelverstone@gmail.com>
2020-04-15 22:11:04 -0500

    Decided to do the different, US/Canda version, instead of the global version as itll be more straightforward and not require as much autoauthentication

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const stripe = require("stripe")(process.env.stripe_secret_key);
 require("dotenv").config();
 import routes from "./routes";
 
@@ -18,6 +19,14 @@ routes(app);
 // Send every request to the React app
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// Setting up the payment intent variable for Stripe.js
+const paymentIntent = await stripe.paymentIntents.create({
+  amount: 10,
+  currency: 'usd',
+  // Verifying integration 
+  metadata: {integration_check: "accept_a_payment"},
 });
 
 app.listen(PORT, function() {
