751faa0ad7442381342e26240880fc7b02dc4bd1
Sean Belverstone <seanbelverstone@gmail.com>
2020-04-27 23:16:48 -0500

    Installed react-stripe-elements and also created an API.js file within the utils folder. Deciding not to use it for the stripe payment right now as it is required within the file directly, however I will use it when the user needs to be created.

---
+++ client/src/components/CheckoutForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import {
+  CardNumberElement,
+  CardExpiryElement,
+  CardCVCElement,
+  injectStripe
+} from 'react-stripe-elements';
+import axios from 'axios';
+
+const CheckoutForm = ({ selectedProduct, stripe, history }) => {
+	
+	if (selectedProduct === null) history.push('/')
+  
+	const [receiptUrl, setReceiptUrl] = useState('')
+  
+	const handleSubmit = async event => {
+	  event.preventDefault()
+  
+	  const { token } = await stripe.createToken()
+  
+	  const order = await axios.post('http://localhost:7000/api/stripe/charge', {
+		amount: selectedProduct.price.toString().replace('.', ''),
+		source: token.id,
+		receipt_email: 'customer@example.com'
+	  })
+  
+	  setReceiptUrl(order.data.charge.receipt_url)
+	}
+
+	if (receiptUrl) {
+		return (
+		  <div className="success">
+			<h2>Payment Successful!</h2>
+			<a href={receiptUrl}>View Receipt</a>
+		  </div>
+		)
+	  }
+
+	return (
+	<div className="checkout-form">
+		<p>Amount: ${selectedProduct.price}</p>
+		<form onSubmit={handleSubmit}>
+		<label>
+			Card details
+			<CardNumberElement />
+		</label>
+		<label>
+			Expiration date
+			<CardExpiryElement />
+		</label>
+		<label>
+			CVC
+			<CardCVCElement />
+		</label>
+		<button type="submit" className="order-button">
+			Pay
+		</button>
+		</form>
+	</div>
+	)
+}
+	
+	export default injectStripe(CheckoutForm)
