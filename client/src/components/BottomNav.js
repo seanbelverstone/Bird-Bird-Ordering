4f9587ec64bc17d9fa0bf785a27fe6fa99a421a1
Sean Belverstone <seanbelverstone@gmail.com>
2020-04-30 22:08:53 -0500

    Added in links and names for each navitem

--- client/src/components/BottomNav.js
+++ client/src/components/BottomNav.js
@@ -1,16 +1,9 @@
 import React, { useState } from 'react';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
-  NavbarBrand,
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
   NavbarText
 } from 'reactstrap';
 
@@ -24,10 +17,35 @@ const BottomNav = (props) => {
       <Navbar color="light" light expand="md">
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+              <NavLink href="https://www.birdbirdbiscuit.com/birdbirdbiscuit">HOME</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              <NavLink href="https://www.toasttab.com/bird-bird-biscuit/v3">ONLINE ORDER</NavLink>
+            </NavItem>
+            <NavItem>
+              {/* this will be the live website when done */}
+              <NavLink href="#">DOZENS</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.birdbirdbiscuit.com/">OUR STORY</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.birdbirdbiscuit.com/menu">MENU</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.birdbirdbiscuit.com/location">LOCATION</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.birdbirdbiscuit.com/parking">PARKING</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.birdbirdbiscuit.com/contact">CONTACT</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.birdbirdbiscuit.com/press">PRESS</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.birdbirdbiscuit.com/now-hiring">NOW HIRING</NavLink>
             </NavItem>
           </Nav>
           <NavbarText>Simple Text</NavbarText>
