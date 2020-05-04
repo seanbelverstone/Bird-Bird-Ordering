979819179b6e1caa52a29c6d92f1a33ca5ef00d8
Sean Belverstone <seanbelverstone@gmail.com>
2020-05-03 19:47:31 -0500

    Added the handleChange function to telephone and email address

--- client/src/components/UserForm/index.js
+++ client/src/components/UserForm/index.js
@@ -22,18 +22,18 @@ class UserForm extends React.Component {
     return (
       <div>
         <AvForm>
-          <AvField name="nameCustomMessage" label="Name" type="text" onChange={this.handleChange} validate={{
+          <AvField name="name" label="name" type="text" onChange={this.handleChange} validate={{
               required: {value: true, errorMessage: 'Please enter a name'},
               pattern: {value: /^[a-zA-Z ]+$/, errorMessage: 'Your name must be composed only with letter and numbers'},
               minLength: {value: 4, errorMessage: 'Your name must be between 4 and 32 characters'},
               maxLength: {value: 32, errorMessage: 'Your name must be between 4 and 32 characters'}
             }} />
 
-          <AvField name="telephoneProp" label="Phone" type="text" validate={{
+          <AvField name="telephone" label="telephone" type="text" onChange={this.handleChange} validate={{
             tel: true,
             required: {value: true, errorMessage: 'Please enter a phone number'}}} />
 
-          <AvField name="email" label="Email" type="text" validate={{
+          <AvField name="email" label="email" type="text" onChange={this.handleChange} validate={{
             email: true,
             required: {value: true, errorMessage: 'Please enter an email address.'}}} />
 
