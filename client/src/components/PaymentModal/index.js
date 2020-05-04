e5dbb20c340357836a3e70da1400d66476aef917
Sean Belverstone <seanbelverstone@gmail.com>
2020-05-03 19:07:45 -0500

    Made a style.css file for the payment modal to get that font in line

--- client/src/components/PaymentModal/index.js
+++ client/src/components/PaymentModal/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import UserForm from "../UserForm";
+import "./style.css";
 
 const PaymentModal = (props) => {
   const {
