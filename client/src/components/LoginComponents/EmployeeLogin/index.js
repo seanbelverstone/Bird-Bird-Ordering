6692e1d3c50ac568afb71bc73316d13bd0d7e0d6
Sean Belverstone <seanbelverstone@gmail.com>
2020-05-11 21:41:03 -0500

    Turned the functional component, EmployeeLogin into a class component as I need to utilise state. Added in an onCHange function to handle the changing input. Will then compare input with saved password

--- client/src/components/LoginComponents/EmployeeLogin/index.js
+++ client/src/components/LoginComponents/EmployeeLogin/index.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-const EmployeeLogin = (props) => {
+class EmployeeLogin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-  function handleSubmit(event) {
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (event) => {
+		const {name, value} = event.target;
+		this.setState(
+		  {[name]: value}
+		)
+  };
+
+  handleSubmit(event) {
     event.preventDefault();
     console.log("hello")
   }
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <FormGroup>
-        <Label for="username">Username</Label>
-        <Input type="name" name="username" id="username" placeholder="Username" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="password">Password</Label>
-        <Input type="password" name="password" id="password" placeholder="Password" />
-      </FormGroup>
-      <Button>Submit</Button>
-    </Form>
-  );
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <FormGroup>
+          <Label for="username">Username</Label>
+          <Input type="name" 
+                 name="username" 
+                 id="username" 
+                 placeholder="Username" 
+                 onChange={this.handleChange}/>
+
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input type="password" 
+                 name="password" 
+                 id="password" 
+                 placeholder="Password" 
+                 onChange={this.handleChange}/>
+                 
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
 }
 
 export default EmployeeLogin;
