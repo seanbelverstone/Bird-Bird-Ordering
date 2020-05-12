ab8e4feb8531b86d4e925a4b1d7107930c283a81
Sean Belverstone <seanbelverstone@gmail.com>
2020-05-12 17:56:47 -0500

    Installed react-event-calendar to see if this would be a good fit for our use case. Imported it into the orderCalendar component and created a test event. Checking to see if it works

--- client/src/components/LoginComponents/OrderCalendar/index.js
+++ client/src/components/LoginComponents/OrderCalendar/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import EventCalendar from "react-event-calendar";
+
+const events = [
+	{
+		start: "2020-05-12",
+		end: "2020-05-12",
+		title: "test",
+		description: "testing out this event calendar"
+	}
+]
 
 class OrderCalendar extends React.Component {
-	
+
 	render() {
 		return(
-			<h1>Hello, you're logged in! :)</h1>
+			<div>
+				<h1>ORDER CALENDAR</h1>
+				<EventCalendar
+					month={5}
+					year={2020}
+					events={events}
+					onEventClick={(target, eventData, day) => {console.log(eventData)}}
+					/>
+			</div>
+
 		)
 	}
 
