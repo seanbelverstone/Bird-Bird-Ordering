38dc2044d5c372fc0f1054b60b2ee7ab45ef52da
Sean Belverstone <seanbelverstone@gmail.com>
2020-05-12 18:05:36 -0500

    Following the setup instructions on the repo, I imported the react-big-calendar and set up a localizer using date-fns.

--- client/src/components/LoginComponents/OrderCalendar/index.js
+++ client/src/components/LoginComponents/OrderCalendar/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
 
+const locales = {
+  'en-US': require('date-fns/locale/en-US'),
+}
 
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 class OrderCalendar extends React.Component {
 
@@ -8,7 +23,13 @@ class OrderCalendar extends React.Component {
 		return(
 			<div>
 				<h1>ORDER CALENDAR</h1>
-
+				<Calendar
+					localizer={localizer}
+					events={myEventsList}
+					startAccessor="start"
+					endAccessor="end"
+					style={{ height: 500 }}
+					/>
 			</div>
 
 		)
