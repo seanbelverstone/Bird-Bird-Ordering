e5807beff2757157d6f55719acf94bd08480ddda
Sean Belverstone <seanbelverstone@gmail.com>
2020-05-12 18:28:36 -0500

    Seems to be working but the events are going in the wrong slots for some reason. Will look into further

--- client/src/components/LoginComponents/OrderCalendar/index.js
+++ client/src/components/LoginComponents/OrderCalendar/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
+import React from "react";
+import { Calendar, dateFnsLocalizer, Views } from "react-big-calendar"
+import format from "date-fns/format"
+import parse from "date-fns/parse"
+import startOfWeek from "date-fns/startOfWeek"
+import getDay from "date-fns/getDay"
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+let allViews = Object.keys(Views).map(k => Views[k])
 
 const locales = {
-  'en-US': require('date-fns/locale/en-US'),
+  "en-US": require("date-fns/locale/en-US"),
 }
 
 const localizer = dateFnsLocalizer({
@@ -18,14 +21,33 @@ const localizer = dateFnsLocalizer({
 });
 
 class OrderCalendar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			events: [
+				{
+					"title": "test",
+					"start": new Date(2020, 5, 12, 12, 0, 0, 0),
+					"end": new Date(2020, 5, 12, 13, 0, 0, 0),
+					"desc": "First order of the day"
+				}
+			]
+		}
+	}
 
 	render() {
+
+		const events = this.state.events;
+
 		return(
 			<div>
 				<h1>ORDER CALENDAR</h1>
 				<Calendar
 					localizer={localizer}
-					events={myEventsList}
+					events={events}
+					views={allViews}
+					step={60}
+					showMultiDayTimes
 					startAccessor="start"
 					endAccessor="end"
 					style={{ height: 500 }}
