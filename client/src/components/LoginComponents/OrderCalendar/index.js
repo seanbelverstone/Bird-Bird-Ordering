0211675b388eabe8ef4233fd77c14ab183f72036
Sean Belverstone <seanbelverstone@gmail.com>
2020-05-12 18:00:53 -0500

    Removed react-event-calendar as it was coming up with an error that is apparently a current, unresolved issue. Was shown react-big-calendar which actually looks better to use, as it allows week by week views, monthly views and also time schedules

--- client/src/components/LoginComponents/OrderCalendar/index.js
+++ client/src/components/LoginComponents/OrderCalendar/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import EventCalendar from "react-event-calendar";
 
-const events = [
-	{
-		start: "2020-05-12",
-		end: "2020-05-12",
-		title: "test",
-		description: "testing out this event calendar"
-	}
-]
+
 
 class OrderCalendar extends React.Component {
 
@@ -16,12 +8,7 @@ class OrderCalendar extends React.Component {
 		return(
 			<div>
 				<h1>ORDER CALENDAR</h1>
-				<EventCalendar
-					month={5}
-					year={2020}
-					events={events}
-					onEventClick={(target, eventData, day) => {console.log(eventData)}}
-					/>
+
 			</div>
 
 		)
