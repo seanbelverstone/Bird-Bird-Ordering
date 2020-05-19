94bd472ce8764d3ec6d6df44bf948ad039cd5a00
Sean Belverstone <seanbelverstone@gmail.com>
2020-05-18 22:48:00 -0500

    After some shuffling and messing around with the split() function, got the data to render effectively.

--- client/src/components/LoginComponents/EventModal/index.js
+++ client/src/components/LoginComponents/EventModal/index.js
@@ -4,11 +4,26 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 class EventModal extends React.Component {
 
 	sortDescription = (desc) => {
+		// split the description array up by the space and enters that was jumbling it up
 		let splitDesc = desc.split(/[\s ↵↵]+/);
-		let joinedDesc = splitDesc.join(" ");
+		let justNotes = desc.split("Notes: ")
 
-		console.log(joinedDesc);
-		return joinedDesc;
+		// I then use interpolation to make it easier to display each item individually.
+		let quantity = `${splitDesc[1]} ${splitDesc[2]}`;
+		let telephone = `${splitDesc[3]} ${splitDesc[4]}`;
+		let email = `${splitDesc[5]} ${splitDesc[6]}`;
+		let total = `${splitDesc[7]} ${splitDesc[8]}`;
+		let notes = `${splitDesc[9]} ${justNotes[1]}`;
+
+		return (
+			<div>
+				<p>{quantity}</p>
+				<p>{telephone}</p>
+				<p>{email}</p>
+				<p>{total}</p>
+				<p>{notes}</p>
+			</div>
+		)
 	}
 
 
