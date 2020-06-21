f24ff733d3c9ebc58292db50421a9b5900637e7a
Sean Belverstone <seanbelverstone@gmail.com>
2020-06-20 20:52:45 -0500

    Tried to make it so the second part of the event modal has a class, so it will be easier to add a style but i think im going the wrong way about it. Might have to split it up even further, but thats something for another day

--- client/src/components/LoginComponents/EventModal/index.js
+++ client/src/components/LoginComponents/EventModal/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import format from "date-fns/format";
 import CompleteButton from "../CompleteButton";
+import "./style.css";
 
 let splitDesc;
 const regex = /:\d\d([ ap]|$)/
