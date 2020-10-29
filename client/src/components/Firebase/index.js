0714364e9b9b2639697ad875929349c5bffec052
Sean Belverstone <seanbelverstone@gmail.com>
2020-10-29 10:10:25 -0500

    Created a context.js and an index.js file. Created a FirebaseContext class, which ill use to wrap App in, allowing us to use Firebase anywhere in the project - similarly to Redux`s Provider. The index allows for easy exporting

--- client/src/components/Firebase/index.js
+++ client/src/components/Firebase/index.js
@@ -1,32 +1,6 @@
-import app from "firebase/app"
-
-const prodConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_FIREBASE_URL,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
-
-const devConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_DEV_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_DEV_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_FIREBASE_DEV_URL,
-	projectId: process.env.REACT_APP_FIREBASE_DEV_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_DEV_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_DEV_MESSAGING_ID,
-	appId: process.env.REACT_APP_FIREBASE_DEV_APP_ID
-};
-
-// Saving 2 different configs so I dont alter the one in production when testing
-const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
-
-class Firebase {
-	constructor() {
-		app.initializeApp(config)
-	}
-}
+import FirebaseContext from "./context";
+import Firebase from "./firebase";
 
 export default Firebase;
+
+export { FirebaseContext };
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Firebase, {FirebaseContext} from "./components/Firebase";
 import registerServiceWorker from "./registerServiceWorker";
 import "./pages/css/reset.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<FirebaseContext.Provider value={new Firebase()}>
+		<App />
+	</FirebaseContext.Provider>, 
+	document.getElementById("root"));
 registerServiceWorker();
