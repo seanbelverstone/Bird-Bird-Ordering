1dd0dc549cff5d086759a50b212cd2bcb30cd48d
Sean Belverstone <seanbelverstone@gmail.com>
2020-10-29 10:03:28 -0500

    Realised id started working on the firebase integration on the other branch. Created this new one. Made a firebase file, installed it via npm and lined out the config - making sure to add the data into the .env. Also added a ternary operator to use the dev config if not in production, ensuring i dont overwrite the live numbers

--- client/src/components/Firebase/index.js
+++ client/src/components/Firebase/index.js
@@ -1,6 +1,6 @@
 import app from "firebase/app"
 
-const config = {
+const prodConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 	databaseURL: process.env.REACT_APP_FIREBASE_URL,
@@ -8,7 +8,20 @@ const config = {
 	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
 	appId: process.env.REACT_APP_FIREBASE_APP_ID
-  };
+};
+
+const devConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_DEV_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_DEV_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DEV_URL,
+	projectId: process.env.REACT_APP_FIREBASE_DEV_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_DEV_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_DEV_MESSAGING_ID,
+	appId: process.env.REACT_APP_FIREBASE_DEV_APP_ID
+};
+
+// Saving 2 different configs so I dont alter the one in production when testing
+const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
 
 class Firebase {
 	constructor() {
